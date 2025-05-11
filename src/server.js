e4cc4577a853f0a3b8790c83679b9e08bfb18214
Abinsaj <148989575+Abinsaj@users.noreply.github.com>
2025-05-11 21:55:08 +0530

    Issue Resolved

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import cors from 'cors'
 import productRoutes from './Routes/productRoutes.js'
 import ConnectBD from './Config/databaseConfig.js';
 import dotenv from 'dotenv'
+import { errorHandler } from './Middleware/errorHandleMiddleware.js';
 
 dotenv.config()
 
@@ -22,6 +23,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}))
 
 app.use('/api/products',productRoutes)
+
+app.use(errorHandler)
 
 app.listen(PORT,()=>{
     console.log(`serveris running on port ${PORT}`)
